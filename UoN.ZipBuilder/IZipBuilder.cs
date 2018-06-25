a342b795261437c6bb32b46a1fa1cf519a3ba38e
Jonathan Couldridge <jcouldridge@outlook.com>
2018-06-25 14:15:46 +0100

    added interfacing for encryption and zip64 config. also added a bits enum for AES keys

--- UoN.ZipBuilder/IZipBuilder.cs
+++ UoN.ZipBuilder/IZipBuilder.cs
@@ -5,6 +5,55 @@
     /// </summary>
     public interface IZipBuilder
     {
+        #region Init
+
+        /// <summary>
+        /// Initialises a stream to use for the underlying zip data
+        /// this Zipbuilder instance will manipulate.
+        /// </summary>
+        /// <returns>The ZipBuilder instance.</returns>
+        ZipBuilder CreateZipStream();
+
+        #endregion
+
+        #region Config
+
+        /// <summary>
+        /// Implementations of this class default to using Zip64.
+        /// 
+        /// If you need pre-NT6 (Vista) compressed folders or other older code support,
+        /// You should use this directive, as they don't support Zip64.
+        /// 
+        /// This will disallow files larger than 4GB.
+        /// </summary>
+        /// <returns>The ZipBuilder instance.</returns>
+        ZipBuilder DisableZip64();
+
+        /// <summary>
+        /// Add weak password encryption to the zip file.
+        /// 
+        /// This is the original PK Zip encryption spec,
+        /// it's very weak and is NOT RECOMMENDED,
+        /// but is supported by some older zip clients that don't support newer encryption,
+        /// such as Windows Compressed Folders.
+        /// </summary>
+        /// <param name="password">The password for decrypting the zip.</param>
+        /// <returns>The ZipBuilder instance.</returns>
+        ZipBuilder UseClassicEncryption(string password);
+
+        /// <summary>
+        /// Add AES password encryption to the zip file.
+        /// 
+        /// This is WinZip's open zip encryption method, which uses AES.
+        /// </summary>
+        /// <param name="password">The password for decrypting the zip.</param>
+        /// <returns>The ZipBuilder instance.</returns>
+        ZipBuilder UseAesEncryption(string password, AesKeyBits keyStrength = AesKeyBits._256);
+
+        #endregion
+
+        #region Content
+
         /// <summary>
         /// Adds the top level contents of a directory (no resursion) to the zip,
         /// preserving the filenames of the files inside, and adding them to provided entryname.
@@ -30,6 +79,10 @@
         /// <returns>The ZipBuilder instance.</returns>
         ZipBuilder AddTextContent(string content, string entryName);
 
+        #endregion
+
+        #region Output
+
         /// <summary>
         /// Returns the zip represented by the ZipBuilder state as a byte array,
         /// for use elsewhere.
@@ -37,11 +90,6 @@
         /// <returns>A byte array representing the zip.</returns>
         byte[] AsByteArray();
 
-        /// <summary>
-        /// Initialises a stream to use for the underlying zip data
-        /// this Zipbuilder instance will manipulate.
-        /// </summary>
-        /// <returns>The ZipBuilder instance.</returns>
-        ZipBuilder CreateZipStream();
+        #region
     }
 }
