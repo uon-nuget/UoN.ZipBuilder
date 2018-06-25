816575b4610c1483caa4c34abc0a59634882d529
Jonathan Couldridge <jcouldridge@outlook.com>
2018-06-25 13:21:45 +0100

    native .net zipbuilder from decsys

---
+++ UoN.ZipBuilder/ZipBuilder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace UoN.ZipBuilder
+{
+    public class ZipBuilder
+    {
+        private MemoryStream ZipStream { get; set; }
+        private ZipArchive Zip { get; set; }
+
+        public ZipBuilder CreateZipStream()
+        {
+            if (ZipStream != null) throw new InvalidOperationException(
+                "This builder already has a MemoryStream");
+            if (Zip != null) throw new InvalidOperationException(
+                "This builder already has a ZipArchive");
+
+            ZipStream = new MemoryStream();
+
+            Zip = new ZipArchive(ZipStream, ZipArchiveMode.Create, true);
+
+            return this;
+        }
+
+        public ZipBuilder AddFile(string sourcePath, string entryName)
+        {
+            Zip.CreateEntryFromFile(sourcePath, entryName);
+            return this;
+        }
+
+        public ZipBuilder AddDirectoryShallow(string path, string entryName)
+        {
+            if (!Directory.Exists(path))
+                throw new ArgumentException("Directory Path is invalid", nameof(path));
+
+            foreach (var f in Directory.EnumerateFiles(path))
+                Zip.CreateEntryFromFile(f, Path.Combine(entryName, Path.GetFileName(f)));
+
+            return this;
+        }
+
+        public ZipBuilder AddTextContent(string content, string entryName)
+        {
+            var entry = Zip.CreateEntry(entryName);
+
+            using (var entryStream = entry.Open())
+            using (var streamWriter = new StreamWriter(entryStream))
+                streamWriter.Write(content);
+
+            return this;
+        }
+
+        public byte[] AsByteArray()
+        {
+            Zip.Dispose();
+            var result = ZipStream.ToArray();
+            ZipStream.Dispose();
+            return result;
+        }
+    }
+}
